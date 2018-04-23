fb41c6da2f3a2e486962dfff57fd6bc665daa805
Mike Chu and Matt Miller <matt@>
2018-04-23 11:57:34 -0700

    Update index.html and main.js to create basic user interface.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,40 @@
-// import $ from 'jquery';
-// import { function } from './filename';
+import $ from 'jquery';
+import { Tamagotchi } from './tamagotchi.js';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+
+$(document).ready(function() {
+  let tamagotchi;
+
+  $('form').submit(function(e) {
+    e.preventDefault();
+    tamagotchi = new Tamagotchi($('#name').val());
+    tamagotchi.setAge();
+    tamagotchi.setFoodLevel();
+    tamagotchi.setAttentionLevel();
+    tamagotchi.setSleepLevel();
+
+    $('#tamagotchi .petname').text($('form .petname').val());
+    setInterval(function() {
+      $('#food-level span').text(tamagotchi.foodLevel);
+      $('#attention-level span').text(tamagotchi.attentionLevel);
+      $('#sleep-level span').text(tamagotchi.sleepLevel);
+    }, 1000);
+
+    $('form').hide();
+    $('#tamagotchi').show();
+  });
+
+  $('#feed').click(function() {
+    tamagotchi.feed();
+  });
+
+  $('#play').click(function() {
+    tamagotchi.playTime();
+  });
+
+  $('#sleep').click(function() {
+    tamagotchi.sleep();
+  });
+});
